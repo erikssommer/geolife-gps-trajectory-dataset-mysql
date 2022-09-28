00cb1fd51a783f468b1f6280b528a1cef8859931
Lavrans Bjerkestrand <lavrans.bjerkestrand@hotmail.com>
2022-09-28 16:00:23 +0200

    Added queries 1, 2, 5, 6a and 6b

--- queryExecution.py
+++ queryExecution.py
@@ -11,25 +11,66 @@ class QueryExecution:
         return self.cursor.fetchall()
 
     # Query 1
+    def query1(self):
+        return [
+            self.execute_query("SELECT COUNT(*) FROM geolife.`User`"),
+            self.execute_query("SELECT COUNT(*) FROM geolife.Activity"),
+            self.execute_query("SELECT COUNT(*) FROM geolife.TrackPoint")
+        ]
 
     # Query 2
+    def query2(self):
+        query = """
+            SELECT AVG(count) AS average_number_of_activities
+            FROM (
+                SELECT user_id, COUNT(*) AS count
+                FROM geolife.Activity
+                GROUP BY user_id
+            ) AS user_activity_count
+        """
+        self.execute_query(query)
 
     # Query 3
     def top_twenty_users(self):
         query = """
-        SELECT user_id, COUNT(*) AS num_activities 
-        FROM Activity
-        GROUP BY user_id
-        ORDER BY num_activities
-        DESC LIMIT 20
+            SELECT user_id, COUNT(*) AS num_activities 
+            FROM Activity
+            GROUP BY user_id
+            ORDER BY num_activities
+            DESC LIMIT 20
         """
         self.execute_query(query)
 
     # Query 4
+    def query4(self):
+        query = """
+            SELECT transportation_mode, COUNT(transportation_mode) as count
+            FROM geolife.Activity
+            WHERE transportation_mode!=""
+            GROUP BY transportation_mode
+        """
+        self.execute_query(query)
 
     # Query 5
     
     # Query 6
+    def query6a(self):
+        query = """
+            SELECT YEAR(start_date_time) AS year, COUNT(id) AS count
+            FROM Activity
+            GROUP BY year
+            LIMIT 1
+        """
+        self.execute_query(query)
+
+    def query6b(self):
+        query = """
+            SELECT YEAR(start_date_time) AS year, SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(end_date_time, start_date_time)))) AS duration
+            FROM Activity
+            GROUP BY year
+            LIMIT 1
+        """
+        self.execute_query(query)
 
     # Query 7
 
