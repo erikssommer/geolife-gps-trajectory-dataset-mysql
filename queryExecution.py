22b5d1a1f7a7b4663be7e72c84a1a5e1b7653573
Lavrans Bjerkestrand <lavrans.bjerkestrand@hotmail.com>
2022-09-28 17:13:43 +0200

    Add query 11

--- queryExecution.py
+++ queryExecution.py
@@ -113,10 +113,18 @@ class QueryExecution:
     # Query 9
 
     # Query 10
+    def query10(self):
+        query = """
+            SELECT user_id, COUNT(TrackPoint.id) as 'trackpoints in forbidden city'
+            FROM TrackPoint
+            JOIN Activity ON TrackPoint.activity_id = Activity.id
+            WHERE lat >= 39.916 AND lat < 39.917
+            AND lon >= 116.397 AND lon < 116.398
+            GROUP BY Activity.user_id
+        """
+        self.execute_query(query)
 
     # Query 11
-
-    # Query 12
 
 def main():
     query = QueryExecution()
